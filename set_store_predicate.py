c3c4a27fc595ca432952435d6c8c3c76d6fe54d1
StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>
2019-12-06 13:46:22 +0800

    update set_store_predicate: upload

---
+++ set_store_predicate.py
@@ -0,0 +1,21 @@
+import tvm
+
+n = tvm.var("n")
+A = tvm.placeholder((n), name='A')
+k = tvm.reduce_axis((0, n), 'k')
+B = tvm.compute((1,), lambda i: tvm.sum(A[k], axis=k), name='B')
+
+s = tvm.create_schedule(B.op)
+
+ko, ki = s[B].split(B.op.reduce_axis[0], factor=16)
+BF = s.rfactor(B, ki)
+tx = tvm.thread_axis("threadIdx.x")
+s[BF].compute_at(s[B], s[B].op.reduce_axis[0])
+
+print(tvm.lower(s, [A, B], simple_mode=True))
+print("---------cutting line---------")
+
+s[B].set_store_predicate(tx.var.equal(0))
+
+print(tvm.lower(s, [A, B], simple_mode=True))
+exit(0)
