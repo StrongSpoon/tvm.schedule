197eb5ac4c40decefe20de357a293ff145c820c6
StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>
2019-12-06 12:52:28 +0800

    update compute_inline: upload

---
+++ compute_inline.py
@@ -0,0 +1,32 @@
+import tvm
+
+n = tvm.var('n')
+k = tvm.var('k')
+pad = tvm.var('pad')
+A = tvm.placeholder((n, n), name='A')
+W = tvm.placeholder((k, k), name='W')
+m = (n - k + 2 * pad) + 1
+Apad = tvm.compute((n + 2 * pad, n + 2 * pad),
+                lambda yy, xx: tvm.if_then_else(
+                    tvm.all(yy >= pad, yy < pad + n, xx >= pad, xx < pad + n), 
+                    A[yy - pad, xx - pad], tvm.const(0., "float32")),
+                    name='Apad')
+
+ry = tvm.reduce_axis((0, k), name='ry')
+rx = tvm.reduce_axis((0, k), name='rx')
+
+B = tvm.compute((m, m),
+                lambda yy, xx: 
+                    tvm.sum(Apad[yy + ry, xx + rx] * W[ry, rx],
+                    axis=[ry, rx]),
+                    name='B')
+
+s = tvm.create_schedule(B.op)
+
+print(tvm.lower(s, [A, W, B], simple_mode=True))
+print("---------cutting line---------")
+
+s[Apad].compute_inline()
+
+print(tvm.lower(s, [A, W, B], simple_mode=True))
+exit(0)
