d2abee3c97a0f014665c198ba2b9a139088ac8f7
StrongSpoon <35829812+StrongSpoon@users.noreply.github.com>
2019-12-06 13:16:26 +0800

    update parallel
    delete useless statement

--- parallel.py
+++ parallel.py
@@ -3,7 +3,6 @@ n = 1024
 m = 1024
 
 A = tvm.placeholder((n, m), name='A')
-# k = tvm.reduce_axis((0, n), name='k')
 l = tvm.reduce_axis((0, m), name = 'l')
 
 B = tvm.compute((n,), lambda i: tvm.sum(A[i, l], axis=l), name='B')
